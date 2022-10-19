c94e31f8c840ae57a8df6fbce3266035aef6f8b2
lavo7873 <linhuang1101@gmail.com>
2022-10-19 16:54:01 -0700

    update

---
+++ homework1/try.java
@@ -0,0 +1,19 @@
+final class Animal {
+      private final String type;
+      private final int age;
+
+      // constructor
+      Animal(String type, int age) {
+            this.type = type;
+            this.age = age;
+      }
+
+      // getters
+      public String getType() {
+            return this.type;
+      }
+
+      public int getAge() {
+            return this.age;
+      }
+}
