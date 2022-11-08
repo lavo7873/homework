c8bd15851bdf5058fab06176128d4b6ea0dce7c3
lavo7873 <linhuang1101@gmail.com>
2022-11-08 15:43:14 -0800

    update

--- homework1/try.java
+++ homework1/try.java
@@ -1,19 +1,46 @@
-final class Animal {
-      private final String type;
-      private final int age;
+class Person {
+      // attributes
+      private String name;
+      private int age;
 
-      // constructor
-      Animal(String type, int age) {
-            this.type = type;
+      // parameterized constructor
+      public Person(String name, int age) {
+            // assigning values
+            this.name = name;
             this.age = age;
       }
 
-      // getters
-      public String getType() {
-            return this.type;
+      // getter for name
+      public String getName() {
+            return name;
       }
 
+      // getter for age
       public int getAge() {
-            return this.age;
+            return age;
+      }
+
+      // setter for name
+      public void setName(String name) {
+            this.name = name;
+      }
+
+      // setter for age
+      public void setAge(int age) {
+            this.age = age;
+      }
+}
+
+// class Artist inherits from Person
+class Artist extends Person {
+      // attribute
+      private String specialty;
+
+      // parameterized constructor with two string value and one int value
+      public Artist(String name, String specialty, int age) {
+            // call to parent constructor with name and age (attributes)
+            super(name, age);
+            // assigning the specialty
+            this.specialty = specialty;
       }
 }
